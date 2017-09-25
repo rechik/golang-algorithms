cf3ad82d9da6db88ed0ff8c2c90dbff35462aecc
rechik <rechik@inbox.ru>
2017-09-26 01:55:31 +0300

    Binary search

---
+++ binary_search.go
@@ -0,0 +1,28 @@
+package golang_algorithms
+
+// Search and return index of given item
+// If no result then return -1
+func binary_search_ints(list []int, item int) int {
+	var low = 0
+	var high = len(list) - 1
+	var mid int
+	var guess int
+
+	for low <= high {
+		mid = int(uint(low+high) >> 1)
+
+		guess = list[mid]
+
+		if (guess == item) {
+			return mid
+		}
+
+		if (guess > item) {
+			high = mid - 1
+		} else {
+			low = mid + 1
+		}
+	}
+
+	return -1
+}
---
+++ binary_search_test.go
@@ -0,0 +1,42 @@
+package golang_algorithms
+
+import (
+	"testing"
+	"sort"
+)
+
+var data = []int{0: -10, 1: -5, 2: 0, 3: 1, 4: 2, 5: 3, 6: 5, 7: 7, 8: 11, 9: 100, 10: 110, 11: 150, 12: 1000, 13: 10000}
+
+var tests = []struct {
+	name string
+	n    int
+	i    int
+}{
+	{"-10 0", -10, 0},
+	{"-5 1", -5, 1},
+	{"0 2", 0, 2},
+	{"1 3", 1, 3},
+	{"100 9", 100, 9},
+	{"666 -1", 666, -1},
+}
+
+func TestSearch(t *testing.T) {
+	for _, e := range tests {
+		i := binary_search_ints(data, e.n)
+		if i != e.i {
+			t.Errorf("%s: expected index %d; got %d", e.name, e.i, i)
+		}
+	}
+}
+
+func BenchmarkBinarySearchInts(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		binary_search_ints(data, 666)
+	}
+}
+
+func BenchmarkStandartSearch(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		sort.SearchInts(data, 666)
+	}
+}
